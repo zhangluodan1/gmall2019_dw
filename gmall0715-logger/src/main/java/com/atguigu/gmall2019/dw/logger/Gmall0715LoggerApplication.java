73c6b19335954f70693ad483c784e7e881df5e1e
zhangluodan <51687697@qq.com>
2019-12-13 12:02:33 +0800

    Initial commit

---
+++ gmall0715-logger/src/main/java/com/atguigu/gmall2019/dw/logger/Gmall0715LoggerApplication.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall2019.dw.logger;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gmall0715LoggerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gmall0715LoggerApplication.class, args);
+    }
+
+}
---
+++ gmall0715-logger/src/test/java/com/atguigu/gmall2019/dw/logger/Gmall0715LoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atguigu.gmall2019.dw.logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gmall0715LoggerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
